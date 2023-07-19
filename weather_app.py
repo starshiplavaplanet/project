7bbd9e266457851d61eef5a50827d6f7a739c9e7
starshiplavaplanet <128258337+starshiplavaplanet@users.noreply.github.com>
2023-07-19 18:36:02 +0300

    Update weather_app.py

--- weather_app.py
+++ weather_app.py
@@ -1,25 +1,56 @@
 import requests
+import tkinter as tk
+from tkinter import messagebox
 
-# Prompt user to enter the preferred city
-city = input("Please enter the city: ")
+def get_weather():
+    city = city_entry.get()
 
-# Set up the API URL and parameters using the user's input for the city
-url = "https://api.openweathermap.org/data/2.5/weather"
-params = {"q": city, "appid": "2f209a89043105a7763a733831a1bf7a", "units": "metric"}
+    url = "https://api.openweathermap.org/data/2.5/weather"
+    params = {"q": city, "appid": "YOUR API KEY", "units": "metric"}
 
-# Make the API request
-response = requests.get(url, params=params)
-weather_data = response.json()
+    response = requests.get(url, params=params)
+    weather_data = response.json()
 
-# Extract the relevant information from the response
-description = weather_data["weather"][0]["description"]
-temperature = weather_data["main"]["temp"]
-humidity = weather_data["main"]["humidity"]
-wind_speed = weather_data["wind"]["speed"]
+    if response.status_code == 200:
+        description = weather_data["weather"][0]["description"]
+        temperature = weather_data["main"]["temp"]
+        humidity = weather_data["main"]["humidity"]
+        wind_speed = weather_data["wind"]["speed"]
 
-# Print the weather information
-print(f"Current weather in {city}: {description.capitalize()}")
-print(f"Temperature: {temperature} °C")
-print(f"Humidity: {humidity}%")
-print(f"Wind speed: {wind_speed} m/s")
+        result_text.set(f"Current weather in {city}: {description.capitalize()}\n"
+                        f"Temperature: {temperature} °C\n"
+                        f"Humidity: {humidity}%\n"
+                        f"Wind speed: {wind_speed} m/s")
+    else:
+        result_text.set("City not found or an error occurred. Please try again.")
 
+# Create the main GUI window
+root = tk.Tk()
+root.title("Weather Checker")
+root.geometry("1200x720")
+
+# Customize colors
+bg_color = "#f0f0f0"  # Light gray background
+text_color = "#333333"  # Dark gray text color
+button_color = "#4CAF50"  # Green button color
+
+# Set the background color
+root.configure(bg=bg_color)
+
+# Create and configure the input elements
+city_label = tk.Label(root, text="Please enter the city:", bg=bg_color, fg=text_color, font=("Arial", 14))
+city_label.pack(pady=5)
+
+city_entry = tk.Entry(root, font=("Arial", 14))
+city_entry.pack(pady=5)
+
+get_weather_button = tk.Button(root, text="Get Weather", command=get_weather, bg=button_color, fg="white", font=("Arial", 14))
+get_weather_button.pack(pady=10)
+
+# Create and configure the output element
+result_text = tk.StringVar()
+result_label = tk.Label(root, textvariable=result_text, bg=bg_color, fg=text_color, font=("Arial", 14))
+result_label.pack(pady=10)
+
+# Run the GUI event loop
+root.mainloop()
